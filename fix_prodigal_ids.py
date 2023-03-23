67da3dcb8478a69833fe58969efc2c8d3253eacf
Max-Emil Schoen <9975295+maxemil@users.noreply.github.com>
2023-03-23 10:23:04 +0100

    add printf option

--- fix_prodigal_ids.py
+++ fix_prodigal_ids.py
@@ -7,9 +7,10 @@ import click
 @click.command()
 @click.argument('fasta', type=str, nargs=-1)
 @click.option('--gff', type=str)
+@click.option('-p', '--printstyle', type=str, default='03d')
 
-def main(fasta, gff):
-    old2new = fix_gff(gff)
+def main(fasta, gff, printstyle):
+    old2new = fix_gff(gff, printstyle)
     for f in fasta:
         fix_fasta(f, old2new)
 
@@ -25,7 +26,7 @@ def fix_fasta(infile, old2new):
     with open(infile, 'w') as out:
             SeqIO.write(recs, out, 'fasta')
 
-def fix_gff(infile):
+def fix_gff(infile, printstyle):
     lines = []
     old2new = {}
     for line in open(infile):
@@ -33,9 +34,10 @@ def fix_gff(infile):
         if not line.startswith("#"):
             llist = line.split()
             old_id = re.search('ID=([0-9]+)_([0-9]+);', line)
-            new_id = re.sub(old_id.group(), "ID={}_{:03d};".format(llist[0], int(old_id.group(2))), old_id.group())
+            new_id_inner = "{0}_{1:{printstyle}}".format(llist[0], int(old_id.group(2)),printstyle=printstyle) 
+            new_id = "ID={};".format(new_id_inner)
             line = re.sub(old_id.group(), new_id, line)
-            line += "locus_tag={}_{:03d};".format(llist[0], int(old_id.group(2)))
+            line += "locus_tag={};".format(new_id_inner)
             lines.append(line)
             old2new[old_id.group()] = new_id
     with open(infile, 'w') as out:
