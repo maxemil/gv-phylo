485fe4087dc22651ced7884bc5013711b6e64d2e
Max Emil Schoen <9975295+maxemil@users.noreply.github.com>
2023-03-29 08:56:08 +0200

    Add separator option

--- fix_prodigal_ids.py
+++ fix_prodigal_ids.py
@@ -8,9 +8,10 @@ import click
 @click.argument('fasta', type=str, nargs=-1)
 @click.option('--gff', type=str)
 @click.option('-p', '--printstyle', type=str, default='03d')
+@click.option('-s', '--sep', type=str, default='_')
 
-def main(fasta, gff, printstyle):
-    old2new = fix_gff(gff, printstyle)
+def main(fasta, gff, printstyle, sep):
+    old2new = fix_gff(gff, printstyle, sep)
     for f in fasta:
         fix_fasta(f, old2new)
 
@@ -19,14 +20,14 @@ def fix_fasta(infile, old2new):
     for rec in SeqIO.parse(infile, 'fasta'):
         for o, n in old2new.items():
             if o in rec.description:
-                fixed_description = re.sub(o, n, rec.description.replace('{} '.format(rec.id), ''))
-                rec.id = n.replace('ID=', '').replace(';', '')
+                fixed_description = re.sub(o, "ID={};".format(n), rec.description.replace('{} '.format(rec.id), ''))
+                rec.id = n
                 rec.description = fixed_description
         recs.append(rec)
     with open(infile, 'w') as out:
             SeqIO.write(recs, out, 'fasta')
 
-def fix_gff(infile, printstyle):
+def fix_gff(infile, printstyle, sep):
     lines = []
     old2new = {}
     for line in open(infile):
@@ -34,12 +35,12 @@ def fix_gff(infile, printstyle):
         if not line.startswith("#"):
             llist = line.split()
             old_id = re.search('ID=([0-9]+)_([0-9]+);', line)
-            new_id_inner = "{0}_{1:{printstyle}}".format(llist[0], int(old_id.group(2)),printstyle=printstyle) 
+            new_id_inner = "{0}{1}{2:{printstyle}}".format(llist[0], sep, int(old_id.group(2)),printstyle=printstyle) 
             new_id = "ID={};".format(new_id_inner)
             line = re.sub(old_id.group(), new_id, line)
             line += "locus_tag={};".format(new_id_inner)
             lines.append(line)
-            old2new[old_id.group()] = new_id
+            old2new[old_id.group()] = new_id_inner
     with open(infile, 'w') as out:
         for line in lines:
             print(line, file=out)
