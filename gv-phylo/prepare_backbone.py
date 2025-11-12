0b929f1e3f0dad6a6b5fe86f188608d6fdb2793a
Max Emil Schoen <9975295+maxemil@users.noreply.github.com>
2025-11-12 12:26:29 +0100

    add protein ids to csv output

--- gv-phylo/prepare_backbone.py
+++ gv-phylo/prepare_backbone.py
@@ -51,6 +51,8 @@ def make_selection(df, selectors, ingroup, outgroup_per_family, ingroup_per_fami
     return sel
 
 def write_selection(sel, seeds):
+    sel_prot = pd.DataFrame(columns=sel.columns)
+    sel_prot['Protein_ID'] = []
     with open('{}.selection.faa'.format(os.path.basename(seeds).split('.')[0]), 'w') as out:
         for rec in SeqIO.parse(seeds, 'fasta'):
             genome = '_'.join(rec.id.split('..')[0].split('_')[0:-1])
@@ -60,6 +62,7 @@ def write_selection(sel, seeds):
                 rec.id = f'{genome_tax}..{prot}'
                 rec.description = ""
                 SeqIO.write(rec, out, 'fasta')
+                sel_prot = pd.concat([sel_prot, sel[sel['clean_name'] == genome].assign(Protein_ID=rec.id)])
     order2color = {
         'Imitervirales': '#F0E442',
         'Algavirales': '#0072B2',
@@ -68,8 +71,8 @@ def write_selection(sel, seeds):
         'Pimascovirales': '#CC79A7',
         'Pandoravirales': '#df0a0a'
     }
-    sel['Color'] = sel.apply(lambda x: order2color[x['Order']] if x['Order'] in order2color else '#000000', axis=1)
-    sel.to_csv('{}.selection.tsv'.format(os.path.basename(seeds).split('.')[0]), sep='\t', index=False)
+    sel_prot['Color'] = sel_prot.apply(lambda x: order2color[x['Order']] if x['Order'] in order2color else '#000000', axis=1)
+    sel_prot.to_csv('{}.selection.tsv'.format(os.path.basename(seeds).split('.')[0]), sep='\t', index=False)
 
 if __name__ == '__main__':
     main()
